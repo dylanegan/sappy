c7a9233972619bc731ae1e4c6f0110738a406c3f
Dylan Egan <me@dylanegan.com>
2008-12-15 09:40:50 +1100

    Stub out other actions.

--- lib/sappy/actions.rb
+++ lib/sappy/actions.rb
@@ -11,5 +11,29 @@ module Sappy
     def self.enable_monitor(id)
       Sappy::Request.new('enablemonitor', "MonitorID=#{id}").perform
     end
+
+    def self.account_information
+      raise "Not yet implemented."
+    end
+
+    def self.add_monitor
+      raise "Not yet implemented."
+    end
+
+    def self.remove_monitor
+      raise "Not yet implemented."
+    end
+
+    def self.daily_statistics
+      raise "Not yet implemented."
+    end
+
+    def self.monthly_statistics
+      raise "Not yet implemented."
+    end
+
+    def self.annual_statistics
+      raise "Not yet implemented."
+    end
   end
 end
