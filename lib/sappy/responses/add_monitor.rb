6ea69c064332bb614764c5e78f1416a40cc59c86
Dylan Egan <me@dylanegan.com>
2009-02-05 14:48:52 +1100

    Store the ID from the response so its accessible. Change error message.

--- lib/sappy/responses/add_monitor.rb
+++ lib/sappy/responses/add_monitor.rb
@@ -1,13 +1,15 @@
 module Sappy
   module Responses
     class AddMonitor < Response
+      attr_reader :id
       def success(hash)
+        @id = hash["monitor"].first["id"]
       end
 
       def failure(code, message)
         case code
         when "WRONG_DATA"
-          raise ArgumentError, "You didn't provide a correct monitor id: #{message}"
+          raise ArgumentError, "You didn't provide the correct data: #{message}"
         end
       end
     end
