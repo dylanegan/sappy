8c33e2b82be53e90a58e7d705739031311805e71
Dylan Egan <me@dylanegan.com>
2009-02-05 14:59:10 +1100

    Update monitor bacon to suit newness.

--- bacon/monitor_bacon.rb
+++ bacon/monitor_bacon.rb
@@ -5,35 +5,35 @@ module Sappy
     before do
       @account = Account.login("siteuptime-test@engineyard.com", "monitorey")
       @account.monitors.each { |m| m.destroy }
-      @account.add_monitor("awesome", "http", "sf", "spork.in/awesome", "60")
-      @monitor = @account.monitors.first
+      @monitor = @account.add_monitor({:name => "awesome", :service => "http", :location => "sf", :host_name => "spork.in/awesome", :check_period => "60"})
     end
 
     describe "an active monitor" do
       before do
-        @monitor.enable
+        @monitor.enable!
       end
 
       it "can be disabled" do
-        @monitor.disable
-        @monitor.active.should == "no"
+        @monitor.disable!
+        @monitor.should.not.be.enabled
       end
     end
 
     describe "an inactive monitor" do
       before do
-        @monitor.disable
+        @monitor.disable!
       end
 
       it "can be enabled" do
-        @monitor.enable
-        @monitor.active.should == "yes"
+        @monitor.enable!
+        @monitor.should.be.enabled
       end
     end
 
     describe "a monitor" do
       it "can be destroyed" do
         lambda { @monitor.destroy }.should.not.raise
+        @account.monitors.size.should == 0
       end
     end
   end
