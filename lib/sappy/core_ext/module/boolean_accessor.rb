a63e8688e8f505255d9bffac73ebc1eb2f41c1bb
Dylan Egan <me@dylanegan.com>
2009-02-18 20:21:45 +1100

    Add on as a possible value. Mainly for checkbox actions.

--- lib/sappy/core_ext/module/boolean_accessor.rb
+++ lib/sappy/core_ext/module/boolean_accessor.rb
@@ -5,7 +5,7 @@ module Sappy
         syms.each do |sym|
           class_eval(<<-EOS, __FILE__, __LINE__)
             def #{sym}?; @#{sym} == 1; end
-            def #{sym}=(value); @#{sym} = value.to_s.match(/true|yes|1/i) ? 1 : 0; end
+            def #{sym}=(value); @#{sym} = value.to_s.match(/true|yes|on|1/i) ? 1 : 0; end
             def #{sym}; @#{sym}; end
           EOS
         end
